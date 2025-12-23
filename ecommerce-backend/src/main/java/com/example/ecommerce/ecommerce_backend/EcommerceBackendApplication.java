c58a3fe377498e0f894aa72cb1a8745f3cecfc08
Quốc Sang <149033042+SangHq005@users.noreply.github.com>
2025-12-23 08:26:15 +0700

    feat(misc): add misc

---
+++ ecommerce-backend/src/main/java/com/example/ecommerce/ecommerce_backend/EcommerceBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.ecommerce.ecommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class EcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackendApplication.class, args);
+	}
+
+}
